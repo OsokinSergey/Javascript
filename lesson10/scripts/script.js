0501ee8df7452f146eef0c802cb059b07e36506e
Sergey Osokin <saosokin@gmail.com>
2023-07-30 20:23:13 +0300

    init lesson10

---
+++ lesson10/scripts/script.js
@@ -0,0 +1,89 @@
+
+//Задание 1
+const numbers = {
+    keyin1: 1,
+    keyin2: 2,
+    keyin3: 3,
+    keyin4: 4,
+    keyin5: 5,
+    keyin6: 6,
+    keyin7: 7,
+    }
+console.log(Object.values(numbers).filter(val=>val>3));
+
+//Задание 2
+
+const post = {
+        author: "John", // вывести этот текст
+        postId: 23,
+        comments: [
+        {
+            userId: 10,
+            userName: "Alex",
+            text: "lorem ipsum",
+            rating: {
+            likes: 10,
+            dislikes: 2, // вывести это число
+            },
+        },
+        {
+            userId: 5, // вывести это число
+            userName: "Jane",
+            text: "lorem ipsum 2", // вывести этот текст
+            rating: {
+                likes: 3,
+                dislikes: 1,
+            },
+        },
+        ],
+    };
+
+//Задание 3
+
+const products = [
+    {
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+    ];
+
+products.forEach(el=>el.price*=0.85)
+
+//Задание
+
+const products1 = [
+    {
+        id: 3,
+        price: 127,
+        photos: [
+            "1.jpg",
+            "2.jpg",
+        ],
+    },
+    {
+        id: 5,
+        price: 499,
+        photos: [],
+    },
+    {
+        id: 10,
+        price: 26,
+        photos: [
+            "3.jpg",
+        ],
+    },
+    {
+        id: 8,
+        price: 78,
+    },
+    ];
+    
+    
